f7c0d1e48355dd92e789ef395292f86d1eb1db8c
Alex Solo <alex.n.solo@gmail.com>
2018-03-20 16:41:56 -0600

    ruby samples

---
+++ ruby/samples/basic.rb
@@ -0,0 +1,20 @@
+# First, follow the installation instructions at https://github.com/intrinio/ruby-sdk
+
+# Load the gem
+require 'intrinio-sdk'
+
+# Setup authorization
+Intrinio.configure do |config|
+  config.api_key['api-key'] = 'YOUR API KEY'
+end
+
+company_api = Intrinio::CompanyApi.new
+
+begin
+  results = company_api.filter_companies()
+  results.each do |company|
+    puts company.name
+  end
+rescue Intrinio::ApiError => e
+  puts "Exception when calling CompanyApi->filter_companies: #{e}"
+end
---
+++ ruby/samples/screener_basic.rb
@@ -0,0 +1,45 @@
+# First, follow the installation instructions at https://github.com/intrinio/ruby-sdk
+
+# Load the gem
+require 'intrinio-sdk'
+
+# Setup authorization
+Intrinio.configure do |config|
+  config.api_key['api-key'] = 'YOUR API KEY'
+end
+
+security_api = Intrinio::SecurityApi.new
+
+opts = { 
+  logic: Intrinio::SecurityScreenGroup.new(
+    operator: "AND",
+    clauses: [
+      # Marketcap >= $50m
+      Intrinio::SecurityScreenClause.new(
+        field: "marketcap",
+        operator: "gte",
+        value: 50_000_000,
+      ),
+      # PE < 3
+      Intrinio::SecurityScreenClause.new(
+        field: "pricetoearnings",
+        operator: "lt",
+        value: 3,
+      )
+    ]
+  ),
+  order_column: "marketcap",
+  order_direction: "desc",
+  primary_only: true,
+}
+
+begin
+  results = security_api.screen_securities(opts)
+  results.each do |result|
+    marketcap = result.data.find{|x| x.tag == "marketcap"}.number_value
+    pe = result.data.find{|x| x.tag == "pricetoearnings"}.number_value
+    puts "#{result.security.name} | Marketcap: #{marketcap} | P/E: #{pe}"
+  end
+rescue Intrinio::ApiError => e
+  puts "Exception when calling SecurityApi->screen_securities: #{e}"
+end
