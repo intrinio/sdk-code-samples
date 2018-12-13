49862adb75b1c17e224f1ebb4116004553f09c02
Kenneth Miller <kgmilerdev@gmail.com>
2018-12-13 16:17:58 -0600

    python sample update

--- python/basic.py
+++ python/basic.py
@@ -1,15 +1,24 @@
 from __future__ import print_function
 import time
+from datetime import datetime, date, timedelta
 import intrinio_sdk
 from intrinio_sdk.rest import ApiException
 from pprint import pprint
 
-intrinio_sdk.ApiClient().configuration.api_key['api-key'] = 'YOUR_API_KEY' 
+intrinio_sdk.ApiClient().configuration.api_key['api_key'] = 'YOUR_API_KEY'
 
-company_api = intrinio_sdk.CompanyApi()
+security_api = intrinio_sdk.SecurityApi()
+
+identifier = 'AAPL' # str | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
+tag = 'adj_close_price' # str | An Intrinio data tag ID or code-name
+type = '' # str | Filter by type, when applicable (optional)
+start_date = str(date.today() - timedelta(days = 90)) # date | Get historical data on or after this date (optional)
+end_date = str(date.today()) # date | Get historical date on or before this date (optional)
+sort_order = 'desc' # str | Sort by date `asc` or `desc` (optional)
+next_page = '' # str | Gets the next page of data from a previous API call (optional)
 
 try:
-    api_response = company_api.filter_companies()
+    api_response = security_api.get_security_historical_data(identifier, tag, type=type, start_date=start_date, end_date=end_date, sort_order=sort_order, next_page=next_page)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling CompanyApi->filter_companies: %s\n" % e)
+    print("Exception when calling SecurityApi->get_security_historical_data: %s\n" % e)
