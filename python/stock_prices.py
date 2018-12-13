e9d28a3db7d2b7912203ceaee5070f82003effb4
Kenneth Miller <kgmilerdev@gmail.com>
2018-12-13 17:06:08 -0600

    update python example to stock_prices

--- python/stock_prices.py
+++ python/stock_prices.py
@@ -10,15 +10,13 @@ intrinio_sdk.ApiClient().configuration.api_key['api_key'] = 'YOUR_API_KEY'
 security_api = intrinio_sdk.SecurityApi()
 
 identifier = 'AAPL' # str | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
-tag = 'adj_close_price' # str | An Intrinio data tag ID or code-name
-type = '' # str | Filter by type, when applicable (optional)
 start_date = str(date.today() - timedelta(days = 90)) # date | Get historical data on or after this date (optional)
 end_date = str(date.today()) # date | Get historical date on or before this date (optional)
-sort_order = 'desc' # str | Sort by date `asc` or `desc` (optional)
+frequency = 'daily' # str | Sort by date `asc` or `desc` (optional)
 next_page = '' # str | Gets the next page of data from a previous API call (optional)
 
 try:
-    api_response = security_api.get_security_historical_data(identifier, tag, type=type, start_date=start_date, end_date=end_date, sort_order=sort_order, next_page=next_page)
+    api_response = security_api.get_security_stock_prices(identifier, start_date=start_date, end_date=end_date, frequency=frequency)
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling SecurityApi->get_security_historical_data: %s\n" % e)
