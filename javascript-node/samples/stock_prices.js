5120260d0ce26c986bdcab2e2722bc1049c5dc40
Kenneth Miller <kgmilerdev@gmail.com>
2018-12-15 11:20:38 -0600

    add stock_prices.js as example

---
+++ javascript-node/samples/stock_prices.js
@@ -0,0 +1,21 @@
+var intrinio = require('intrinio');
+const util = require('util')
+
+intrinio.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = "OjA0N2ZmZGFkZGE0MDk5ZjI0YzZmYjM2NzI3OTgzN2I1";
+
+var security_api = new intrinio.SecurityApi();
+
+var identifier = "AAPL"; // String | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
+
+var opts = { 
+  'startDate': new Date("2018-01-01"), // Date | Return prices on or after the date
+  'endDate': new Date("2019-01-01"), // Date | Return prices on or before the date
+  'frequency': "daily", // String | Return stock prices in the given frequency
+  'nextPage': null // String | Gets the next page of data from a previous API call
+};
+
+security_api.getSecurityStockPrices(identifier, opts).then(function(data) {
+  console.log(util.inspect(data, false, null, true));
+}, function(error) {
+  console.error(error);
+});
