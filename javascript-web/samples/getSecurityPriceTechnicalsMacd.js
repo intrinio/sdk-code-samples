13613c1ba2a3133f3c26005d0b58b44afa74091a
Alex <alexandersmith223@gmail.com>
2019-02-07 15:51:04 -0500

    JavaScript web code samples for bb, macd, rsi, sma

---
+++ javascript-web/samples/getSecurityPriceTechnicalsMacd.js
@@ -0,0 +1,34 @@
+var intrinioSDK = require('intrinio-sdk');
+intrinioSDK.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = "YOUR API KEY";
+ 
+var securityAPI = new intrinioSDK.SecurityApi();
+ 
+var identifier = "AAPL"; // String | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
+ 
+var opts = { 
+  'startDate': "2019-01-01", // String | Return technical indicator values on or after the date
+  'endDate': "2019-01-31", // String | Return technical indicator values on or before the date
+  'timezone': "America/New_York", // String | Returns technical indicators in this timezone
+};
+
+securityAPI.getSecurityPriceTechnicalsBb(identifier, opts).then(function(data) {
+  $('#apiResult').html(JSON.stringify(data, undefined, 2));
+  var indicator = data.indicator;
+  var security = data.security;
+  var technicals = data.technicals;
+  
+  console.log('Technicals for ' + security.ticker);
+  console.log(technicals.length + ' values for ' + indicator.name + ' returned!');
+  console.log();
+
+  technicals.forEach(function(technical){
+    console.log("DateTime:    " + technical.date_time);
+    console.log("Lower Band:  " + technical.value.lower_band);
+    console.log("Middle Band: " + technical.value.middle_band);
+    console.log("Upper Band:  " + technical.value.upper_band);
+    console.log("--------------------------------------------------------------------------------");
+  });
+  
+}, function(error) {
+  console.error(error);
+});
---
+++ javascript-web/samples/getSecurityPriceTechnicalsSma.js
@@ -0,0 +1,30 @@
+var intrinioSDK = require('intrinio-sdk');
+intrinioSDK.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = "YOUR API KEY";
+ 
+var securityAPI = new intrinioSDK.SecurityApi();
+ 
+var identifier = "AAPL"; // String | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
+ 
+var opts = {
+  'pageSize': 25 // Number | The number of results to return
+};
+
+securityAPI.getSecurityPriceTechnicalsSma(identifier, opts).then(function(data) {
+  $('#apiResult').html(JSON.stringify(data, undefined, 2));
+  var indicator = data.indicator;
+  var security = data.security;
+  var technicals = data.technicals;
+
+  console.log('Technicals for ' + security.ticker);
+  console.log(technicals.length + ' values for ' + indicator.name + ' returned!');
+  console.log();
+
+  technicals.forEach(function(technical){
+    console.log("DateTime: " + technical.date_time);
+    console.log("SMA:      " + technical.value);
+    console.log("--------------------------------------------------------------------------------");
+  });
+  
+}, function(error) {
+  console.error(error);
+});
